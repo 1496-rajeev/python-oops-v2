dd2061b79ce251b73804ec1991ea4303da8fc593
Rajeev Verma <rajeevverma@Rajeevs-MacBook-Pro.local>
2025-08-03 12:26:42 +0530

    operator overloading

---
+++ operator-overloading.py
@@ -0,0 +1,7 @@
+# operator overloading means one operator perform different task with different datatype
+
+print(2 + 5) # addition
+print([2,4,5,6] + [5,8])  # Merge
+print("2" + "5") # concatenate
+
+# it also shows polymorphism here Plus is reprenting different form itself.
